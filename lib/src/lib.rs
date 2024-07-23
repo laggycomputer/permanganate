02c0655a34f63b6aca38d741bd4ace9f76272ce2
Dante Dam <laggycomputer@yahoo.com>
2024-07-23 00:12:13 -0700

    fix panic when trying to traverse out of integer bounds (e.g. up on (0, 0))

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -107,10 +107,10 @@ impl NumberlinkBoard {
 
     pub fn step(&self, loc: Location, direction: BoardTraverseDirection) -> Option<Location> {
         let new_loc = match direction {
-            BoardTraverseDirection::UP => (loc.0 + 0, loc.1 - 1),
-            BoardTraverseDirection::DOWN => (loc.0 + 0, loc.1 + 1),
-            BoardTraverseDirection::LEFT => (loc.0 - 1, loc.1 + 0),
-            BoardTraverseDirection::RIGHT => (loc.0 + 1, loc.1 + 0),
+            BoardTraverseDirection::UP => (loc.0.overflowing_add_signed(0).0, loc.1.overflowing_add_signed(-1).0),
+            BoardTraverseDirection::DOWN => (loc.0.overflowing_add_signed(0).0, loc.1.overflowing_add_signed(1).0),
+            BoardTraverseDirection::LEFT => (loc.0.overflowing_add_signed(-1).0, loc.1.overflowing_add_signed(0).0),
+            BoardTraverseDirection::RIGHT => (loc.0.overflowing_add_signed(1).0, loc.1.overflowing_add_signed(0).0),
         };
 
         match (0..self.dims.0).contains(&new_loc.0) && (0..self.dims.1).contains(&new_loc.1) {
