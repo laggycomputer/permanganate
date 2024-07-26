f413704ded3a2e21bbb7a6016f629d0ba9d429a7
Dante Dam <laggycomputer@yahoo.com>
2024-07-25 23:32:37 -0700

    better binary crate main file for testing

--- solver/src/main.rs
+++ solver/src/main.rs
@@ -1,12 +1,12 @@
-use unordered_pair::UnorderedPair;
-
 use permanganate::NumberlinkBoard;
 
 fn main() {
-    let mut board = NumberlinkBoard::with_dims((3, 3));
-    board.add_termini(UnorderedPair::from(((0, 0), (0, 2))));
-    board.add_termini(UnorderedPair::from(((1, 0), (1, 2))));
-    board.add_termini(UnorderedPair::from(((2, 0), (2, 2))));
+    let mut board = NumberlinkBoard::with_dims((5, 5));
+    board.add_termini_with_display('A', ((0, 0), (1, 4)));
+    board.add_termini_with_display('B', ((2, 0), (1, 3)));
+    board.add_termini_with_display('C', ((2, 1), (2, 4)));
+    board.add_termini_with_display('D', ((4, 0), (3, 3)));
+    board.add_termini_with_display('E', ((4, 1), (3, 4)));
 
     println!("{board}");
     let solved = board.solve_bsat().unwrap();
