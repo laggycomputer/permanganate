61adb739911aa4f99c09043255d2887a803dc916
Dante Dam <laggycomputer@yahoo.com>
2024-07-28 12:25:59 -0700

    bye bye affiliation struct

--- lib/src/common/affiliation.rs
+++ lib/src/common/affiliation.rs
@@ -1,7 +1,1 @@
 pub type AffiliationID = usize;
-
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct Affiliation {
-    pub(crate) ident: AffiliationID,
-    pub(crate) display: char,
-}
